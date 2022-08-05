1d787a1afa18c6458e44a88712dd49a72d392084
Jehloni <Justjehloni@gmsil.com>
2022-08-05 14:47:57 -0400

    Add Javascript, added events like onclick, ripple effect, keypress, mouseover, mouseleave, and added some blur

--- javascript.js
+++ javascript.js
@@ -17,24 +17,110 @@ productNameHandle = document.getElementById("productName");
 productNameHandle.onclick = function() {
     alert( "You like clicking things huh? 🤔" );
 }
-ListOfCategories = document.getElementById("mainContentListOfCategories");
+//  = document.getElementById("productName");
+// let rippleEffect = document.getElementById("ripple1");
+// rippleEffect.addEventListener("click", ()=> {
+//     let styles = rippleEffect.style;
+//     if(styles.opacity == 0) {
+//         styles.transform = "scale(10)";
+//     } else if (styles.opacity == 1) {
+//         styles.transform = "scale(0)";
+//     }
 
-ListOfCategories.onclick = function() {
-    alert("uh oh, why did u do that?")
-}
-// -------------
-itemdef = document.getElementById("itemdefinition");
+//     console.log("button was clicked ")
+// })
 
-itemdef.onclick = function() {
-    alert("So, you want to know more? click on the ingredients")
-}
-ingredients = document.getElementById("ingredients");
+	// let btn = document.querySelector(".btn");
+    const btn = document.querySelectorAll(".btn");
+    for(let index = 0; index < btn.length; index++) {
+        btn[index].addEventListener("click", ripple);
+    }
 
-ingredients.onclick = function() {
-    alert("Any suggestiond to the making of the products? Email us!")
-}
-headername = document.getElementById("headername");
+    function ripple(e) {
+        // Create span element
+		let ripple = document.createElement("span");
 
-headername.onclick = function() {
-    alert("Welcome to Jehneva!")
-}
+		// Add ripple class to span
+		ripple.classList.add("ripple");
+
+		// Add span to the button
+		this.appendChild(ripple);
+
+		// Get position of X
+		let x = e.clientX - e.target.offsetLeft;
+
+		// Get position of Y
+		let y = e.clientY - e.target.offsetTop;
+
+		// Position the span element
+		ripple.style.left = `${x}px`;
+		ripple.style.top = `${y}px`;
+
+		// Remove span after 0.3s
+		setTimeout(() => {
+			ripple.remove();
+		}, 300);
+
+    }
+
+	// Listen for click event
+	// btn.onclick = function (e) {
+
+		// Create span element
+		// let ripple = document.createElement("span");
+
+		// Add ripple class to span
+		// ripple.classList.add("ripple");
+
+		// Add span to the button
+		// this.appendChild(ripple);
+
+		// Get position of X
+		// let x = e.clientX - e.target.offsetLeft;
+
+		// Get position of Y
+		// let y = e.clientY - e.target.offsetTop;
+
+		// Position the span element
+		// ripple.style.left = `${x}px`;
+		// ripple.style.top = `${y}px`;
+
+		// Remove span after 0.3s
+		// setTimeout(() => {
+			// ripple.remove();
+		// }, 300);
+
+	// };
+
+
+	const productName = document.getElementById("productName");
+		// to change the color the text
+		productName.addEventListener('mouseover', function handleMouseOver() {
+			productName.style.color = 'red';
+		  });
+
+	const body = document.querySelector('body');
+	body.addEventListener('keypress', keyj);
+	function keyj(e){
+		var screenPositionY = window.scrollY;
+		if(screenPositionY == 377 && e.charCode == 108) {
+			document.querySelector("footer a").click();
+		}
+		// else {
+		// 	console.log(`You pressed the ${e.key} key! ${e.charCode} and the screen position was ${window.scrollY}`);
+		// }
+	}
+
+	// grab image
+	// add event listener
+	// create function to blur
+	var productPicture = document.getElementById("productPicture");
+	productPicture.addEventListener('mouseover', mainpicBlur);
+	productPicture.addEventListener('mouseleave', mainpicUnblur);
+
+	function mainpicBlur() {
+		productPicture.style = "filter: blur(30px)";
+	}
+	function mainpicUnblur() {
+		productPicture.style = "filter: blur(0)";
+	}
